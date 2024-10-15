83740150c85f67be2ff9beec3e291da683b1b3ba
BraisVarela <braisvarela6@gmail.com>
2024-10-15 12:30:42 +0200

    Ejercicio 5 Hecho

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -2,6 +2,13 @@ package com.campusdual.classroom;
 
 public class Exercise05 {
     public static void main(String[] args) {
+        int n1 = 0;
+        int n2 = -10;
+        checkLessOrGreaterEqual(n1,n2);
+        checkMultiply(n1,n2);
+        checkPositiveNegative(n1);
+
+
 
     }
 
@@ -10,12 +17,24 @@ public class Exercise05 {
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
 
+        if (n1 < n2){
+            System.out.println(n1 + " es menor que " + n2);
+        } else {
+            System.out.println(n1 + " es mayor o igual que " + n2);
+
+        }
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
+        if (n1 % n2 == 0){
+            System.out.println(n1 + " es múltiplo de " + n2);
+        } else {
+            System.out.println(n1 + " no es múltiplo de " + n2);
+
+        }
 
     }
 
@@ -23,6 +42,11 @@ public class Exercise05 {
     // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
     // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
     public static void checkPositiveNegative(int num) {
-        
+        if (num <= -1){
+            System.out.println(num + " es negativo.");
+        } else {
+            System.out.println(num + " es positivo.");
+
+        }
     }
 }
